572480ce558ccc737db49685170772676a71f8fe
sonnyzxc <sonnyku9@gmail.com>
2024-02-03 16:12:09 +0000

    Initial Commit

---
+++ omu/omuApp.swift
@@ -0,0 +1,17 @@
+//
+//  omuApp.swift
+//  omu
+//
+//  Created by Sonny Ku on 03/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct omuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
